f7f914be89fb1ac1b093e7e0de1f0d2debbc2b2a
Tyler Miele <tylermiele10@gmail.com>
2022-04-28 18:09:49 -0400

    fixed JS file.

--- js/myJS.js
+++ js/myJS.js
@@ -10,8 +10,8 @@ const hoverElem = document.querySelector("#div1");
 const hoverElem2 = document.querySelector("section");
 
 function normal(){
-    pageBg.setAttribute("style", "background-image: url(../a3/images/movieposterBlurred.jpg)");
-    pageBg3.setAttribute("style", "background-image: url(../a3/images/movieposterNoText.jpg)");
+    pageBg.setAttribute("style", "background-image: url(images/movieposterBlurred.jpg)");
+    pageBg3.setAttribute("style", "background-image: url(images/movieposterNoText.jpg)");
     pageBg2.setAttribute("style", "background-color: rgba(16,3,23,255)");
 
     /*this is another way to style muliple components of a single element. Code Snippit: https://stackoverflow.com/questions/3968593/how-can-i-set-multiple-css-styles-in-javascript*/
@@ -21,24 +21,24 @@ function normal(){
     
 }
 function alt(){
-    pageBg.setAttribute("style", "background-image: url(../a3/images/movieposterALTBlurred.jpg)");
-    pageBg3.setAttribute("style", "background-image: url(../a3/images/movieposterALTNoText.jpg)");
+    pageBg.setAttribute("style", "background-image: url(images/movieposterALTBlurred.jpg)");
+    pageBg3.setAttribute("style", "background-image: url(images/movieposterALTNoText.jpg)");
     pageBg2.setAttribute("style", "background-color: rgb(0,0,0");
     document.getElementById("title").style.cssText = "color: rgba(3,255,140,255); text-shadow: 2px 3px 4px rgb(209, 208, 208);";
     document.getElementById("title1").style.cssText = "color: rgba(3,255,140,255); text-shadow: 2px 3px 4px rgb(209, 208, 208);";
     document.getElementById("title2").style.cssText = "color: rgba(3,255,140,255); text-shadow: 2px 3px 4px rgb(209, 208, 208);";
 }
 function sectionNormal(){
-    pageBg.setAttribute("style", "background-image: url(../a3/images/movieposterBlurred.jpg)");
-    pageBg3.setAttribute("style", "background-image: url(../a3/images/movieposterNoText.jpg)")
+    pageBg.setAttribute("style", "background-image: url(images/movieposterBlurred.jpg)");
+    pageBg3.setAttribute("style", "background-image: url(images/movieposterNoText.jpg)")
     pageBg2.setAttribute("style", "background-color: rgba(16,3,23,255)");
     document.getElementById("title").style.cssText = "color: rgb(255, 145, 0); text-shadow: 2px 3px 4px red;";
     document.getElementById("title1").style.cssText = "color: rgb(255, 145, 0); text-shadow: 2px 3px 4px red;";
     document.getElementById("title2").style.cssText = "color: rgb(255, 145, 0); text-shadow: 2px 3px 4px red;";
 }
 function sectionAlt(){
-    pageBg.setAttribute("style", "background-image: url(../a3/images/movieposterALTBlurred.jpg)");
-    pageBg3.setAttribute("style", "background-image: url(../a3/images/movieposterALTNoText.jpg)");
+    pageBg.setAttribute("style", "background-image: url(images/movieposterALTBlurred.jpg)");
+    pageBg3.setAttribute("style", "background-image: url(images/movieposterALTNoText.jpg)");
     pageBg2.setAttribute("style", "background-color: rgb(0,0,0");
     document.getElementById("title").style.cssText = "color: rgba(3,255,140,255); text-shadow: 2px 3px 4px rgb(209, 208, 208);";
     document.getElementById("title1").style.cssText = "color: rgba(3,255,140,255); text-shadow: 2px 3px 4px rgb(209, 208, 208);";
